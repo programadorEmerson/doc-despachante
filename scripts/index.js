9c1246c8dc5f6e2e5d7d01093722a3bef9acbb21
Emerson Saturnino <programador.emerson@hotmail.com>
2021-08-04 22:04:07 -0300

    Começo de Layout considerendo resolução 1024x768 para web, porém responsivo em porcentagem

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,4 @@
   
 window.onload = function () {
-  const ImgMatheus = '../assets/images/matheus.png';
-
-  // Conteúdo Header
-  const header_img = document.querySelector('#header');
-  console.log(header_img);
 
 }
